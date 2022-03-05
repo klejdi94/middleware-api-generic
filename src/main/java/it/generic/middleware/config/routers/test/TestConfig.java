80d59c8a3daf10cb10b857441df9d673ff301634
Klejdi 2K <klejdikristo@gmail.com>
2022-03-06 00:20:08 +0100

    Added welcome page

--- src/main/java/it/generic/middleware/config/routers/test/TestConfig.java
+++ src/main/java/it/generic/middleware/config/routers/test/TestConfig.java
@@ -30,25 +30,37 @@ public class TestConfig {
     private final TestHandler testHandler;
 
     /**
-     *
      * @return
      */
     @Bean
-    public RouterFunction<ServerResponse> createTestRouterFunction(){
+    public RouterFunction<ServerResponse> createTestRouterFunction() {
         return route().path(apiConfig.getApiPath(),
-                builder -> builder.nest(accept(MediaType.APPLICATION_JSON),builder2 -> builder2
+                builder -> builder.nest(accept(MediaType.APPLICATION_JSON), builder2 -> builder2
                         .path(Paths.ROOT,
-                            builderDomains -> builderDomains
-                                .GET("",testHandler::get)
-                                .POST("",testHandler::add)
-                                .PATCH(Paths.BY_ID,testHandler::updateById)
-                                .GET(Paths.BY_ID,testHandler::getById)
-                                .DELETE("",testHandler::deleteAll)
-                                .DELETE(Paths.BY_ID,testHandler::deleteById)
+                                builderDomains -> builderDomains
+                                        .GET("", testHandler::get)
+                                        .POST("", testHandler::add)
+                                        .PATCH(Paths.BY_ID, testHandler::updateById)
+                                        .GET(Paths.BY_ID, testHandler::getById)
+                                        .DELETE("", testHandler::deleteAll)
+                                        .DELETE(Paths.BY_ID, testHandler::deleteById)
+                        ).path("",
+                                builderDomains -> builderDomains
+                                        .GET("", testHandler::getHello)
                         )
                 )).build();
     }
 
+    @Bean
+    public RouterFunction<ServerResponse> createWelcomeRouterFunction() {
+        return route().path("",
+                builder -> builder.nest(accept(MediaType.APPLICATION_JSON), builder2 -> builder2
+                        .path("",
+                                builderDomains -> builderDomains
+                                        .GET("", testHandler::getHello)
+                        )
+                )).build();
+    }
 
 
     @UtilityClass
--- src/main/java/it/generic/middleware/handler/test/TestHandler.java
+++ src/main/java/it/generic/middleware/handler/test/TestHandler.java
@@ -8,7 +8,12 @@ import it.generic.middleware.model.GenericEntityI;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
 
 @RestController
 @AllArgsConstructor
@@ -55,4 +60,10 @@ public class TestHandler extends AbstractGenericEntityHandler {
         return TestConfig.Paths.Parameters.ID;
     }
 
+    public Mono<ServerResponse> getHello(ServerRequest serverRequest) {
+        return ServerResponse
+                .status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue("Welcome, start using this middleware demo project, start at /api/v1/test , Enjoy!");
+    }
 }
